2c0807092e01587bc5b8a32a16c13ecdf516943b
My_Oh_My <longhuicode@foxmail.com>
2018-06-10 23:23:07 +0800

    创建了一个 MessageService，以便在类之间实现松耦合通讯。
    HeroService 连同注入到它的服务 MessageService 一起，注入到了组件中。

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import {Hero} from '../class/hero';
 import {HEROES} from '../mock/mock-heroes';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes.');
     return of(HEROES);
   }
 }
