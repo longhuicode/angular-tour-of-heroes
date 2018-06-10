3726a64714bd085a22625685578967488331a59f
My_Oh_My <longhuicode@foxmail.com>
2018-06-10 22:26:48 +0800

    使用依赖注入 herService 获取英雄列表.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../../class/hero';
-import {HEROES} from '../../mock/mock-heroes';
+import {HeroService} from '../../service/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,18 @@ import {HEROES} from '../../mock/mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero): void {
