5ab8b5ff0281f56586cac8f4479a946a96c01441
My_Oh_My <longhuicode@foxmail.com>
2018-06-10 17:10:08 +0800

    导入 FormsModule,使用双向绑定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
